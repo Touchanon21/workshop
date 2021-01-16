b03a445918f86c20133181578e804c68b1e95289
touchanon21 <thatchanon.m@ku.th>
2021-01-16 11:06:08 +0700

    ตัวอย่าง

---
+++ string/concatenation.py
@@ -0,0 +1,11 @@
+# case1
+a = "Hello"
+b = "World"
+c = a + b
+print(c)  # output : HelloWorld
+
+# case2
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)  # output : Hello World
---
+++ string/f_string.py
@@ -0,0 +1,5 @@
+name = "Por"
+age = 26
+
+result = f"My name is {name}, and I am {age}"
+print("result:", result)
